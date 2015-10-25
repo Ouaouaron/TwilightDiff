199022efa6a94ad18926b261de2517834caa35db
Brad Aylsworth <Brad.Aylsworth@gmail.com>
2015-10-25 00:44:41 -0500

    first draft written

--- twilightdiff/twilightdiff.py
+++ twilightdiff/twilightdiff.py
@@ -5,16 +5,72 @@ class Prose:
         self.text = []
         with open(location, encoding='utf-8') as file:
             for line in file:
-                self.text += line.split()
+                # Separate words and keep (but isolate) line endings
+                self.text += line.split() 
                 self.text.append('\n')
-        self.head = 0
+
+    def diff(self, altered):
+        taggedtext = []
+        LOOKAHEAD = 30
+        altindex = 0
+
+        result = ProseDiff()
+        for word in self.text:
+            matched = False
+            if word == '\n':
+                for i in range(LOOKAHEAD)
+                    if altered[altindex+i] == '\n':
+                        result.push(altered[altindex:altindex+i], 'altered')
+                        result.push('', 'newline')
+                        altindex += i + 1
+                        matched = True
+                        break
+                continue
+            for i in range(LOOKAHEAD):
+                if word.lower() == altered[altindex+i].lower():
+                    result.push(altered[altindex:altindex+i], 'altered')
+                    result.push(word, 'same')
+                    altindex += i + 1
+                    mathced = True
+                    break
+            if matched == False:
+                result.push(word, 'original')
+                continue
+
 
 
 class ProseDiff:
-    def __init__(self, taggedtext):
-        """`taggedtext` should be a list of (text, tag) pairs where `tag` is
-           a string 'same', 'original', 'altered', or 'newline'"""
-        self.taggedtext = taggedtext
+    def __init__(self):
+        self.taggedtext = []
+        # variables for push()
+        self.originaltext = ''
+        self.alteredtext = ''
+        self.sametext = ''
+
+    def push(text, tag):
+        if tag == 'newline':
+            self.flush()
+            taggedtext.append( ('', 'newline') )
+        elif tag == 'same':
+            if self.sametext == '':
+                self.flush()
+            self.sametext += ' ' + text
+        elif tag == 'original':
+            self.originaltext += ' ' + text
+        elif tag == 'altered':
+            self.alteredtext += ' ' + text
+        else:
+            raise TagError(tag, text)
+
+            
+    def flush():
+        if self.originaltext != '':
+            taggedtext.append( (self.originaltext.lstrip(), 'original') )
+        if self.alteredtext != '':
+            taggedtext.append( (self.alteredtext.lstrip(), 'altered') )
+        if self.sametext != '':
+            taggedtext.append( (self.sametext.lstrip(), 'same') )
+
 
     def html(self):
         """Return an html file of the diff as a string."""
@@ -34,8 +90,8 @@ class ProseDiff:
                 result += '</p>\n\t<p>'
             elif tag == 'same':
                 result += text
-            elif tag == 'original' || tag == 'altered':
-                result += '<span class="{}">{}</span>'.format(tag, text)
+            elif tag in ['original', 'altered']:
+                result += '<span class="{}"> {} </span>'.format(tag, text)
             else:
                 raise TagError(tag, text)
         result += "</p>\n</article>\n</body>\n</html>"
